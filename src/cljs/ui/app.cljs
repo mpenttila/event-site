3b2b3f3712690be7ec94030f5d8bca39282fbbc9
Markus Penttila <markus.penttila@solita.fi>
2015-10-04 21:23:09 +0300

    Add example content on the page

--- src/cljs/ui/app.cljs
+++ src/cljs/ui/app.cljs
@@ -1,5 +1,6 @@
 (ns ui.app
   (:require [ui.utils :as utils]
+            [ui.content.example :as example]
             [reagent.core :as reagent]
             [goog.events :as events]
             [goog.events.EventType :as EventType]))
@@ -8,12 +9,16 @@
 
 (def offsets (reagent/atom {}))
 
-(def pages ["home" "portfolio" "about" "contact"])
+(def content-ratom (reagent/atom []))
+
+(defn load-content [content-vec]
+  (reset! content-ratom content-vec))
 
 (def content-section
   (with-meta
-    (fn [id]
-      [:div {:id id}])
+    (fn [id markup]
+      [:div.content-section {:id id}
+       markup])
     {:component-did-mount (fn [this]
                             (let [node (reagent/dom-node this)
                                   top (.-offsetTop node)
@@ -29,7 +34,6 @@
         duration 1201.0
         epsilon (/ 1000 60 duration 4)
         ease-fn (utils/bezier 0.25 1 0.25 1 epsilon)]
-
     (.requestAnimationFrame js/window (fn [start]
                                         (letfn [(animate-frame [ts] (let [dt (- ts start)
                                                                           progress (* 1.05 (/ dt duration))
@@ -40,30 +44,37 @@
                                                                         (.requestAnimationFrame js/window animate-frame))))]
                                           (animate-frame start))))))
 
+(defn link-text [id]
+  (apply str (.toUpperCase (first id)) (rest id)))
+
 (def main-view
   (with-meta
     (fn []
-      (let [scroll-y @cur-scroll-y]
-        [:div.app-container
-         [:div.menu-wrapper
-          [:nav.menu
-           [:ul
-            (doall
-              (for [id pages]
-                (let [{:keys [top bottom]} (get @offsets id)]
+      (let [scroll-y @cur-scroll-y
+            content @content-ratom]
+        (when (seq content)
+          [:div.app-container
+           [:div.menu-wrapper
+            [:nav.menu
+             [:ul
+              (doall
+                (for [[idk _] content]
+                  (let [id (name idk)
+                        {:keys [top bottom]} (get @offsets id)]
                     ^{:key (str id "-link")} [:li
                                               [:a {:href (str "#" id)
                                                    :class (when (and (>= scroll-y top) (< scroll-y bottom)) "active")
                                                    :on-click (partial handle-link-click id)}
-                                               id]])))]]]
-         [:div.content
-          (doall
-            (for [id pages]
-              ^{:key id} [content-section id]))]]))
+                                               (link-text id)]])))]]]
+           [:div.content
+            (doall
+              (for [[id markup] content]
+                ^{:key (name id)} [content-section (name id) markup]))]])))
     {:component-did-mount (fn [this]
                             (events/listen js/window EventType/SCROLL
                                            (fn [e] (reset! cur-scroll-y (.-scrollY js/window)))))}))
 
 (defn ^:export start []
   (enable-console-print!)
+  (load-content example/pages)
   (reagent/render-component [main-view] (.getElementById js/document "app")))
---
+++ src/cljs/ui/content/example.cljs
@@ -0,0 +1,10 @@
+(ns ui.content.example)
+
+(def pages [[:home [:div
+                    [:h1 "Welcome to my event!"]]]
+            [:work [:div
+                    [:h1 "Some of my work"]]]
+            [:about [:div
+                     [:h1 "This is me"]]]
+            [:contact [:div
+                       [:h1 "How to reach me"]]]])
