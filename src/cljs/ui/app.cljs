f3f80df24ad3ff0d47221c689f6e50918d9a8764
Markus Penttila <markus.penttila@solita.fi>
2015-10-13 00:23:06 +0300

    Update styling, especially for mobile

--- src/cljs/ui/app.cljs
+++ src/cljs/ui/app.cljs
@@ -1,6 +1,6 @@
 (ns ui.app
   (:require [ui.utils :as utils]
-            [ui.content.example.content :refer [pages]]
+            [ui.content.karonkka.content :refer [pages]]
             [reagent.core :as reagent]
             [goog.events :as events]
             [goog.events.EventType :as EventType]
@@ -59,29 +59,38 @@
 
 (def main-view
   (with-meta
-    (fn []
-      (let [scroll-y @cur-scroll-y
-            content @content-ratom]
-        (when (seq content)
-          [:div.app-container
-           [:div.menu-wrapper
-            [:nav.menu
-             [:ul
-              (doall
-                (for [{:keys [id]} content]
-                  (let [{:keys [top bottom]} (get @offsets id)
-                        active? (and (>= scroll-y (- top 40))
-                                     (< scroll-y (- bottom 41)))]
-                    ^{:key (str id "-link")}
-                    [:li
-                     [:a {:href (str "#" id)
-                          :class (when active? "active")
-                          :on-click (partial handle-link-click id)}
-                      (link-text id)]])))]]]
-           [:div.content
-            (doall
-              (for [{:keys [id] :as page} content]
-                ^{:key id} [content-section page]))]])))
+    (let [drawer-open? (reagent/atom false)]
+      (fn []
+        (let [scroll-y @cur-scroll-y
+              content @content-ratom]
+          (when (seq content)
+            (let [menu-links (doall
+                               (for [{:keys [id]} content]
+                                 (let [{:keys [top bottom]} (get @offsets id)
+                                       active? (and (>= scroll-y (- top 40))
+                                                    (< scroll-y (- bottom 41)))]
+                                   ^{:key (str id "-link")}
+                                   [:li
+                                    [:a {:href (str "#" id)
+                                         :class (when active? "active")
+                                         :on-click (fn [e]
+                                                     (reset! drawer-open? false)
+                                                     (handle-link-click id e))}
+                                     (link-text id)]])))]
+              [:div.app-container
+               [:div.menu-wrapper
+                [:nav.menu
+                 [:div.mobile
+                  [:i.icon-menu {:on-click (fn [_] (swap! drawer-open? not)
+                                             true)}]
+                  (when @drawer-open?
+                    [:div.menu-drawer
+                     [:ul menu-links]])]
+                 [:ul.desktop menu-links]]]
+               [:div.content
+                (doall
+                  (for [{:keys [id] :as page} content]
+                    ^{:key id} [content-section page]))]])))))
     {:component-did-mount (fn [_]
                             (events/listen js/window EventType/SCROLL
                                            (fn [e] (reset! cur-scroll-y (.-scrollY js/window)))))}))
